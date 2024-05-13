460fff30280990885dc9b3e23b1be015319d1ce7
Jedrzej Koriat <jedrzej.koriat@gmail.com>
2024-05-13 22:52:05 +0200

    Modified database; Added Creating/Editing/Viewing Details/Deleting Leave Types

---
+++ Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LeaveManagementWebApp.Data;
+using LeaveManagementWebApp.Models;
+
+namespace LeaveManagementWebApp.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
