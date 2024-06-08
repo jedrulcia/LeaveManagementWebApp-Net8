3a2e006d2250be916a20790bd9645f0915d25536
Jedrzej Koriat <jedrzej.koriat@gmail.com>
2024-06-08 20:05:42 +0200

    fix

---
+++ TrainingPlanApp.Data/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TrainingPlanApp.Web.Data
+{
+	public class User : IdentityUser
+	{
+		public string? FirstName {  get; set; }
+		public string? LastName { get; set; }
+		public string? DateOfBith {  get; set; }
+	}
+}
